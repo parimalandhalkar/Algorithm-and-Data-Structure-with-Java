6e920cc95f1c19be8d2cfa8ff30df21589622dba
parimalandhalkar <andhalkar.parimal@gmail.com>
2018-02-01 14:37:56 +0530

    commit

---
+++ DataStructure/src/tree/SizeOfTree.java
@@ -0,0 +1,27 @@
+package tree;
+
+public class SizeOfTree {
+
+	public static int size(Node root)
+	{
+		if(root == null)
+			return 0;
+		
+		return 1 + size(root.left)+ size(root.right);
+	}
+	
+	public static void main(String args[])
+	{
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.left.left.left = new Node(10);
+		
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		
+	  System.out.println("Size :"+size(root));
+	}
+}
